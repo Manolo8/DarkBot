9184dd2fb95f00ac2ccbfb6c14b31859256a1d3b
Pablo Herrera <pabloherrerapalacio@gmail.com>
2020-04-14 21:34:18 +0200

    Remove comment

--- src/main/java/com/github/manolo8/darkbot/Main.java
+++ src/main/java/com/github/manolo8/darkbot/Main.java
@@ -153,7 +153,7 @@ public class Main extends Thread implements PluginListener {
         statsManager.tick();
 
         tickingModule = running && guiManager.canTickModule();
-        if (tickingModule) tickRunning(); // should double check canTickModule()?
+        if (tickingModule) tickRunning();
         else tickLogic(false);
 
         if (!running && (!hero.hasTarget() || !mapManager.isTarget(hero.target))) {
