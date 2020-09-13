420d8fb243a7f214bc8ed40a8f1ef3d841f9bf35
kekawce <albertark1589@gmail.com>
2020-09-13 02:46:30 -0700

    rewrote caregorybar to use map

--- src/main/java/com/github/manolo8/darkbot/core/objects/slotbars/CategoryBar.java
+++ src/main/java/com/github/manolo8/darkbot/core/objects/slotbars/CategoryBar.java
@@ -4,12 +4,14 @@ import com.github.manolo8.darkbot.core.itf.UpdatableAuto;
 import com.github.manolo8.darkbot.core.objects.swf.ObjArray;
 
 import java.util.ArrayList;
+import java.util.EnumMap;
 import java.util.List;
+import java.util.Map;
 
 import static com.github.manolo8.darkbot.Main.API;
 
 public class CategoryBar extends MenuBar {
-    public List<Category> categories = new ArrayList<>();
+    public Map<CategoryType, Category> categories = new EnumMap<>(CategoryType.class);
 
     private ObjArray categoriesArr = ObjArray.ofVector(true);
 
@@ -17,7 +19,10 @@ public class CategoryBar extends MenuBar {
     public void update() {
         super.update();
         this.categoriesArr.update(API.readMemoryLong(address + 56));
-        this.categoriesArr.sync(this.categories, Category::new, null);
+
+        for (int i = 0; i < categoriesArr.getSize(); i++) {
+            categories.computeIfAbsent(CategoryType.get(i), Category::new).update(categoriesArr.getPtr(i));
+        }
     }
 
     public static class Category extends UpdatableAuto {
@@ -26,11 +31,30 @@ public class CategoryBar extends MenuBar {
 
         private ObjArray itemsArr = ObjArray.ofVector(true);
 
+        public Category(CategoryType categoryType) { }
+
         @Override
         public void update() {
             this.categoryId = API.readMemoryString(address, 32);
             this.itemsArr.update(API.readMemoryLong(address + 40));
             this.itemsArr.sync(this.items, Item::new, null);
+        }
+    }
+
+    private enum CategoryType {
+        LASERS(),
+        ROCKETS(),
+        ROCKET_LAUNCHERS(),
+        SPECIAL_ITEMS(),
+        MINES(),
+        CPUS(),
+        BUY_NOW(),
+        TECH_ITEMS(),
+        SHIP_ABILITIES(),
+        DRONE_FORMATIONS();
+
+        static CategoryType get(int ndx) {
+            return CategoryType.values()[ndx];
         }
     }
 }
