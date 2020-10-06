f0f548542ee54df13346a73077993156147ab40e
kekawce <albertark1589@gmail.com>
2020-10-06 10:23:32 -0700

    added refinement gui

--- src/main/java/com/github/manolo8/darkbot/core/objects/RefinementGui.java
+++ src/main/java/com/github/manolo8/darkbot/core/objects/RefinementGui.java
@@ -11,11 +11,11 @@ import static com.github.manolo8.darkbot.Main.API;
 
 public class RefinementGui extends Gui {
 
-    ObjArray basicOresArr      = ObjArray.ofArrObj();
-    ObjArray upgradableOresArr = ObjArray.ofArrObj();
+    private final ObjArray basicOresArr      = ObjArray.ofArrObj();
+    private final ObjArray upgradableOresArr = ObjArray.ofArrObj();
 
-    List<Ore> basicOres = new ArrayList<>();
-    List<Ore> upgradableOres = new ArrayList<>();
+    private final List<Ore> basicOres      = new ArrayList<>();
+    private final List<Ore> upgradableOres = new ArrayList<>();
 
     public Ore get(OreType type) {
         List<Ore> oresListRef = type.attribute == OreType.Attribute.BASIC ? basicOres : upgradableOres;
@@ -32,8 +32,8 @@ public class RefinementGui extends Gui {
         super.update();
         if (address == 0) return;
 
-        basicOresArr.update(API.readMemoryLong(getElementsList(37),184));
-        upgradableOresArr.update(API.readMemoryLong(getElementsList(31),184));
+        basicOresArr.update(API.readMemoryLong(getElementsList(37), 184));
+        upgradableOresArr.update(API.readMemoryLong(getElementsList(31), 184));
 
         basicOresArr.sync(basicOres, Ore::new, null);
         upgradableOresArr.sync(upgradableOres, Ore::new, null);
@@ -58,17 +58,18 @@ public class RefinementGui extends Gui {
         @Override
         public void update() {
             amount = API.readMemoryInt(address, 0xf0);
-
-            if (name != null && !name.isEmpty()) {
-                String processedName = name.replace("ore_", "");
-                fuzzyName = processedName.substring(0, 1).toUpperCase(Locale.ROOT) + processedName.substring(1);
-            }
         }
 
         @Override
         public void update(long address) {
-            if (address != this.address)
+            if (address != this.address) {
                 name = API.readMemoryString(address, 184);
+
+                if (name != null && !name.isEmpty()) {
+                    String processedName = name.replace("ore_", "");
+                    fuzzyName = processedName.substring(0, 1).toUpperCase(Locale.ROOT) + processedName.substring(1);
+                }
+            }
             super.update(address);
         }
     }
